098de5c24ec2c0f8b73661afcc62bd41cd84d583
Ismael <ismalfmp@gmail.com>
2020-07-29 22:20:57 -0500

    online and offline status

--- app/src/main/java/com/example/messengerapp/AdapterClasses/UserAdapter.kt
+++ app/src/main/java/com/example/messengerapp/AdapterClasses/UserAdapter.kt
@@ -12,8 +12,14 @@ import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.messengerapp.MainActivity
 import com.example.messengerapp.MessageChatActivity
+import com.example.messengerapp.ModelClasses.Chat
 import com.example.messengerapp.ModelClasses.Users
 import com.example.messengerapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,8 +28,7 @@ class UserAdapter(
     mContext: Context,
     mUsers: List<Users>,
     isChatCheck: Boolean
-    ):RecyclerView.Adapter<UserAdapter.ViewHolder?>()
-{
+) : RecyclerView.Adapter<UserAdapter.ViewHolder?>() {
     private val mContext: Context
     private val mUsers: List<Users>
     private var isChatCheck: Boolean
@@ -35,7 +40,8 @@ class UserAdapter(
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view: View = LayoutInflater.from(mContext).inflate(R.layout.user_search_item_layout, viewGroup,false)
+        val view: View = LayoutInflater.from(mContext)
+            .inflate(R.layout.user_search_item_layout, viewGroup, false)
         return UserAdapter.ViewHolder(view)
     }
 
@@ -46,24 +52,33 @@ class UserAdapter(
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
         val user: Users = mUsers[i]
         holder.userNameTxt.text = user!!.getUserName()
-        Picasso.get().load(user.getProfile()).placeholder(R.drawable.profile).into(holder.profileImageView)
-
+        Picasso.get().load(user.getProfile()).placeholder(R.drawable.profile)
+            .into(holder.profileImageView)
+        if (isChatCheck) {
+            holder.onlineImageView.visibility =
+                if (user.getStatus() == "online") View.VISIBLE else View.GONE
+            holder.ofllineImageView.visibility =
+                if (user.getStatus() == "offline") View.VISIBLE else View.GONE
+            retrieveLastMessage(user.getUID(), holder.lasMessageTxt)
+        } else {
+            holder.lasMessageTxt.visibility = View.GONE
+            holder.onlineImageView.visibility = View.GONE
+            holder.ofllineImageView.visibility = View.GONE
+        }
         holder.itemView.setOnClickListener {
             val options = arrayOf<CharSequence>(
                 "Enviar Mensaje",
                 "Ver perfil"
-                )
-            val builder : AlertDialog.Builder = AlertDialog.Builder(mContext)
+            )
+            val builder: AlertDialog.Builder = AlertDialog.Builder(mContext)
             builder.setTitle("¿Qué deseas?")
-            builder.setItems(options, DialogInterface.OnClickListener{dialog, position ->
-                if(position == 0)
-                {
+            builder.setItems(options, DialogInterface.OnClickListener { dialog, position ->
+                if (position == 0) {
                     val intent = Intent(mContext, MessageChatActivity::class.java)
                     intent.putExtra("visit_id", user.getUID())
                     mContext.startActivity(intent)
                 }
-                if(position == 1)
-                {
+                if (position == 1) {
 
                 }
             })
@@ -71,8 +86,35 @@ class UserAdapter(
         }
     }
 
-    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
-    {
+    private fun retrieveLastMessage(chatUserId: String?, lasMessageTxt: TextView) {
+        val firebaseUser = FirebaseAuth.getInstance().currentUser
+        val ref = FirebaseDatabase.getInstance().reference.child("Chats")
+        var lastMsg: String = ""
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                for (d in p0.children) {
+                    val chat = d.getValue(Chat::class.java)
+                    if (firebaseUser != null && chat != null)
+                        if (chat.getReceiver() == firebaseUser!!.uid && chat.getSender() == chatUserId || chat.getReceiver() == chatUserId && chat.getSender() == firebaseUser!!.uid)
+                            lastMsg = chat.getMessage()!!
+                }
+                when (lastMsg) {
+                    "" -> lasMessageTxt.text = ""
+                    "Te envié una imagen" -> lasMessageTxt.text = "Imagen"
+                    else -> lasMessageTxt.text = lastMsg
+                }
+                lastMsg = ""
+
+            }
+
+        })
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var userNameTxt: TextView
         var profileImageView: CircleImageView
         var onlineImageView: CircleImageView
--- app/src/main/java/com/example/messengerapp/MainActivity.kt
+++ app/src/main/java/com/example/messengerapp/MainActivity.kt
@@ -142,4 +142,21 @@ class MainActivity : AppCompatActivity() {
             return titles[position]
         }
     }
+
+    private fun updateStatus(status: String) {
+        val ref = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
+        val hashMap = HashMap<String, Any>()
+        hashMap["status"] = status
+        ref.updateChildren(hashMap)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateStatus("online")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        updateStatus("offline")
+    }
 }
